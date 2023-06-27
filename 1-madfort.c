539b3cc679e1fab7ec5b966ae291bcd07074b807
MadelineSey <seyrambadasu@gmail.com>
2023-06-27 06:32:22 -0700

    Betty would be proud

--- 1-madfort.c
+++ 1-madfort.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "madfortshell.h"
 
 /**
-  *total_value - a function that finds total value of two numbers
-  *@x: one value
-  *@y: the other value
-  *Return: Total
+  *total_value - finds the total value of two numbers
+  *@x: first number
+  *@y: second number
+  *Return: the value of the two numbers
   */
 int total_value(int x, int y)
 {
-	int total;
-
-	total = x + y;
-	return (total);
+	return(x + y);
 }
---
+++ madfortshell/simple_shell
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "madfortshell.h"
+
+/**
+  *total_value - a function that finds total value of two numbers
+  *@x: one value
+  *@y: the other value
+  *Return: Total
+  */
+int total_value(int x, int y)
+{
+	int total;
+
+	total = x + y;
+	return (total);
+}
