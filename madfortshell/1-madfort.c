5e66e5ceaf4b4b107cb5393b4172d99129ce60a5
MadelineSey <seyrambadasu@gmail.com>
2023-06-27 06:40:32 -0700

    Betty would be proud

---
+++ madfortshell/1-madfort.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *total_value - finds the total value of two numbers
+  *@x: first value
+  *@y: second value
+  *Return: the total value of two numbers
+  */
+int total_value(int x, int y)
+{
+	return(x + y)
+}
--- madfortshell/madfortshell.h
+++
@@ -1,8 +0,0 @@
-#ifndef MADFORTSHELL_H
-#define MADFORTSHELL_H
- 
-void execute_shell();
- 
-#endif
-
-/*Fortune & Madeline*/
