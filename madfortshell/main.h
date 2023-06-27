00643bb5885b5df5a542c26822b943d348f751a9
MadelineSey <seyrambadasu@gmail.com>
2023-06-27 06:51:13 -0700

    Betty would be proud

--- madfortshell/main.h
+++ madfortshell/main.h
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+int total_value(int x, int y);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 void execmd(char **argv);
 char *get_location(char *command);
